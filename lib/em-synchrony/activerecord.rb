bb41f97ff968bb578bc1cadd27c4c5769551d844
Min-Jae Lee <minjae.lee@gmail.com>
2014-03-24 13:45:48 -0700

    Comment out ActiveRecord patch from EM-Synchrony

--- lib/em-synchrony/activerecord.rb
+++ lib/em-synchrony/activerecord.rb
@@ -4,21 +4,23 @@ require 'active_record/connection_adapters/abstract/connection_pool'
 require 'active_record/connection_adapters/abstract_adapter'
 require 'em-synchrony/thread'
 
-module ActiveRecord
-  module ConnectionAdapters
-    class ConnectionPool
-      def connection
-        _fibered_mutex.synchronize do
-          @reserved_connections[current_connection_id] ||= checkout
-        end
-      end
-
-      def _fibered_mutex
-        @fibered_mutex ||= EM::Synchrony::Thread::Mutex.new
-      end
-    end
-  end
-end
+# To prevent fiber error which occurs when actuverecords are used in defer
+#
+#module ActiveRecord
+#  module ConnectionAdapters
+#    class ConnectionPool
+#      def connection
+#        _fibered_mutex.synchronize do
+#          @reserved_connections[current_connection_id] ||= checkout
+#        end
+#      end
+#
+#      def _fibered_mutex
+#        @fibered_mutex ||= EM::Synchrony::Thread::Mutex.new
+#      end
+#    end
+#  end
+#end
 
 module EM::Synchrony
   module ActiveRecord
